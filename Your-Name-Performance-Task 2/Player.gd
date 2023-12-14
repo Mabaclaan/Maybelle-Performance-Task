3c3cdfc404a0d0b4d032938c7b6736ba70773cd9
Mabaclaan <122130349+Mabaclaan@users.noreply.github.com>
2023-12-14 14:04:29 -0800

    Sound effects added

--- Your-Name-Performance-Task 2/Player.gd
+++ Your-Name-Performance-Task 2/Player.gd
@@ -3,6 +3,9 @@ extends Area2D
 
 @export var Speed = 400
 
+@onready var bg_music = $"../BGmusic"
+@onready var enemy_hit = $"../EnemyHit"
+
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -23,3 +26,5 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	enemy_hit.play()
+	bg_music.stop()
